23928646af112f9a9f50fc3617347c3dbb755396
DanielTobi0 <adejumobidaniel563@gmail.com>
2024-08-10 15:22:04 +0100

    initial commit

---
+++ script.py
@@ -0,0 +1,42 @@
+"""Build a simple LLM application"""
+
+import groq
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+GROP_API_KEY = os.environ.get("GROQ_API_KEY")
+groq_client = groq.Groq(api_key = GROP_API_KEY)
+
+sys_prompt = """You are a helpful virtual assistance. \
+    Your goal is to provide useful and relevant \
+        responses to my query"""
+
+models = [
+    "llama-3.1-405b-reasoning",
+    "llama-3.1-70b-versatile",
+    "llama-3.1-8b-instant",
+    "mixtral-8x7b-32768"
+]
+
+
+def generate(model, query):
+
+    response = groq_client.chat.completions.create(
+        model = model,
+        messages = [
+            {'role': 'system', 'content': sys_prompt},
+            {'role': 'user', 'content': query},
+        ],
+    response_format = {'type': 'text'},
+    temperature = 1.0 
+
+)
+    answer = response.choices[0].message.content
+    return answer
+
+if __name__ == '__main__':
+    query = 'tell me about RNN"s'
+    model = models[1]
+    print(generate(model, query))
